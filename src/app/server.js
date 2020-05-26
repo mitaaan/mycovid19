2986003aa7add904d56ad8aae8dd83997c5a2022
Rupa <rupa333@gmail.com>
2020-05-26 17:05:22 +0530

    masdvsdvdsvxcf

--- src/app/server.js
+++ src/app/server.js
@@ -7,5 +7,6 @@ app.use(express.static(__dirname + '/dist/covid19'));
 app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/covid19/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
