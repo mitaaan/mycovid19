348121d5dae05fbf493ea1b02140e8848ff8e37c
Rupa <rupa333@gmail.com>
2020-05-27 23:42:58 +0530

    rupouuuuupupuuhhgg

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/covid19'));
+app.use(express.static(__dirname + '/dist/my-project-app'));
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/covid19/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/my-project-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
